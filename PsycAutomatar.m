f9d7c1fefd5880f1ed2e3a518e5ce25d91c0c974
chrisoyh <75465737+chrisoyh@users.noreply.github.com>
2021-12-13 12:32:29 +1100

    Update PsycAutomatar.m

--- PsycAutomatar.m
+++ PsycAutomatar.m
@@ -1,33 +1,38 @@
-%% Load data
-M = readcell('InterviewTracker.xlsx','Sheet','MPP Dom Waitlist 2', 'NumHeaderLines',1);
-filename_template = 'Template_Coversheet Domestic.docx';
+% REMINDERS: 
+% * Have all your Referee Reports, Coversheet templates and Excel file containing all the GivenName, 
+% FamilyName and Student ID in the same folder as this script
+% * Ensure no students have the same GivenName and FamilyName
+% * Coversheets must be .docx, Referee Reports must be .pdf, ensure that no other files of these 2 
+% formats are present in the folder
+
+%% Load data (always run this section first)
+M = readcell('InterviewTracker.xlsx','Sheet','MPP Dom Waitlist 2', 'NumHeaderLines',1); % Reads GivenName, LastName and StudentID from Excel
 
 %% Make folders
 N = size(M, 1);
 fullname_ID = cell(N,1);
 for i = 1 : N
-    fullname_ID{i} = [M{i,1} ' ' upper(M{i,2}) ' ' num2str(M{i,3})]; % Create First LAST STUDENT ID
-    mkdir(fullname_ID{i});                                           % Make new folder for student
-    
+    fullname_ID{i} = [M{i,1} ' ' upper(M{i,2}) ' ' num2str(M{i,3})]; % Create cell array with {'GivenName' 'LASTNAME' 'StudentID'}
+    mkdir(fullname_ID{i});                                           % Make new applicant folder
 end
 
-%% Move templates
-fileList_word = dir('*.docx');
+%% Move Coversheets into folders
+fileList_word = dir('*.docx'); % Reads coversheet filenames
 for i = 1 : N
     for ii = 1 : size(fileList_word, 1)
             
             filename_word = fileList_word(ii).name;
             % Compares first and last names to match - also replaces the last name spaces with '-'
             if strcmpi(erase(filename_word, '.docx'), fullname_ID{i}) 
-                movefile(filename_word , fullname_ID{i});
+                movefile(filename_word , fullname_ID{i}); % moves coversheet to the applicants folder
             end
     end
 end
 
 %% Referee reports
-fileList_pdf = dir('*.pdf');
-num_referee = zeros(N,1);
-missing = {};
+fileList_pdf = dir('*.pdf'); % Reads referee reports filenames
+num_referee = zeros(N,1); % Number of referee reports per student 
+missing = {}; % Check this variable in case there are students that are missed from errors
 
 for i = 1 : N
     try 
@@ -39,7 +44,7 @@ for i = 1 : N
 
             % Compares first and last names to match - also replaces the last name spaces with '-'
             if strcmpi(name{1}, strrep([M{i,1}, '-', M{i,2}], ' ', '-')) 
-                num_referee(i) = num_referee(i) + 1;
+                num_referee(i) = num_referee(i) + 1; % Counts the number of referee reports per student
                 filename_rename_referee = ['Referee ', num2str(num_referee(i)), ' - ' M{i,1} ' ' M{i,2} ' ' num2str(M{i,3}) '.pdf'];
                 movefile(filename_referee , filename_rename_referee);
                 movefile(filename_rename_referee , fullname_ID{i});
@@ -52,8 +57,10 @@ for i = 1 : N
     end
 end
 
-%writecell([num2cell(num_referee) M], 'Number of Referees.xlsx');
-return
+% Checks for students that have less than 2 referee reports
+idx_less_than_2_reports = num_referee < 2;
+less_than_2_reports = M{idx_less_than_2_reports,3};
 
-%% Move transcripts
+
+return
 
